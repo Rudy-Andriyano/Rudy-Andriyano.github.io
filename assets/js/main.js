c44687fea62de3c00f6af1e7d119cf40ee72ed88
Rudy-Andriyano <rudyandriyano.smk4tnj@gmail.com>
2023-09-25 13:51:44 +0700

    update 4

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,14 +90,15 @@ modalCloses.forEach((modalClose, index) =>{
 
 
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".portfolio__container", {
    
-    direction: "horizontal",
+    cssmode: true,
     loop: true,
   
     
     pagination: {
       el: ".swiper-pagination",
+      clickable : true,
     },
   
    
@@ -110,27 +111,32 @@ const swiper = new Swiper('.swiper', {
     
   });
 
-const sections =document.querySelectorAll("section[id]");
 
-function scrollActive(){
-    const scrollY= window.pageXOffset;
 
-    sections.forEach((current) =>{
-        const sectionHeight = current.offsetHeight;
-        const sectionTop = current.offsetTop - 50;
-        sectioId = current.gerAttribute("id");
+  const sections = document.querySelectorAll("section[id]");
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document
-            .querySelector(".nav__menu a [href*=" + sectinId+ "]")
-            .classList.add("active-link");
-        }else {
-            document.querySelector(".nav__menu a [href*=" + sectioId +"]")
-            .classList.remove("active-link");
-        }
-    });
-}
-window.addEventListener("scroll".scrollActive);
+  function scrollActive() {
+      const scrollY = window.pageYOffset;
+  
+      sections.forEach((current) => {
+          const sectionHeight = current.offsetHeight;
+          const sectionTop = current.offsetTop-50,
+          sectionId = current.getAttribute("id");
+  
+          if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+              document
+                  .querySelector(".nav__menu a[href*=" + sectionId + "]")
+                  .classList.add("active-link");
+          } else {
+              document
+                  .querySelector(".nav__menu a[href*=" + sectionId + "]")
+                  .classList.remove("active-link");
+          }
+      });
+  }
+  window.addEventListener("scroll", scrollActive);
+
+
 
 function scrollHeader(){
     const nav = document.getElementById("header");
